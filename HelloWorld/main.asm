eab38ea217d7fe5ab97c29b1f3692249204d4195
Waleedk98 <waleed.khaliq66@gmail.com>
2024-05-20 14:55:37 +0200

    XOR

--- HelloWorld/main.asm
+++ HelloWorld/main.asm
@@ -1,22 +1,34 @@
-
 .equ output_bit = 2
 .equ input_bit = 3
 .equ input_bit2 = 4
 
-	sbi DDRD , output_bit
+    ; Set DDRD bit 2 (PD2) as output
+    sbi DDRD, output_bit
+    
+    ; Initialize registers
+    ldi r17, 0x00    ; reset r17
+    ldi r18, 0x00    ; reset r18
 
-sbi DDRD ,2 ; PD2 is output
-ldi r17 ,0 x00 ; reset r17
-ldi r18 ,0 x00 ; reset r18
-start :
-in r16 , PIND ; read in r16
-bst r16 ,3 ; T <- button 1
-bld r17 ,0 ; r17 <- T
-bst r16 ,4 ; T <- button 2
-bld r18 ,0 ; r18 <- T
-eor r17 , r18 ; r17 xor r18
-sbrc r17 ,0 ; skip next instruction if result was 0
-sbi PORTD ,2 ; turn LED on
-sbrs r17 ,0 ; skip next instruction if result was 1
-cbi PORTD ,2 ; turn LED off
-rjmp start ; goto start
+start:
+    ; Read PIND into r16
+    in r16, PIND
+
+    ; Bit Store to T from button 1 (PD3)
+    bst r16, input_bit
+    ; Bit Load from T to r17 bit 0
+    bld r17, 0
+
+    ; Bit Store to T from button 2 (PD4)
+    bst r16, input_bit2
+    ; Bit Load from T to r18 bit 0
+    bld r18, 0
+
+    ; XOR r17 and r18
+    eor r17, r18
+
+    ; Skip if bit 0 in r17 is clear (if result was 0)
+    sbrc r17, 0
+    ; Set bit 2 in PORTD (turn LED on)
+    sbi PORTD, output_bit
+
+    ; Skip if bit 0 in
